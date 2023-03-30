d1b88cc1df9ac201c45165f4ee81fa47d244f5c6
Guldeste <guldeste999@yahoo.com>
2023-03-29 22:33:14 -0400

    added two files

---
+++ app.js
@@ -0,0 +1,22 @@
+// Constructor and super
+console.log('starting app js');
+
+//Data Types.Literals
+const string = 'some string'; //string literal
+const number = 15;//number literal
+const myObject = {my: 'Desa'}; //onbect literal
+const myArray = [1,2,3,4,5]; //array literal
+const isItTrue = true; //boolean literal
+
+//new, constractor and super
+
+const stringWithNewKeyword = new String('some text'); //string 
+const numberWithNewKeyword = new Number(15);//number 
+const myObjectWithNewKeyword = new Object({name: 'Desa'}); //onbect 
+const myArrayWithNewKeyword = new Array(1,2,3,4,5); //array 
+const isItTrueWithNewKeyword = new Boolean(true); //boolean
+HTMLFormControlsCollection.log(stringWithNewKeyword, numberWithNewKeyword, myObjectWithNewKeyword, myArrayWithNewKeyword, isItTrueWithNewKeyword);
+ 
+
+const date = new Date('March 29, 2023');
+console.dir(date);
